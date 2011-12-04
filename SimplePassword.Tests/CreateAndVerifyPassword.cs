157b51b1887351f5d21cc91e115e2394c9b91a4b
Jimmie <jimmy2saints@gmail.com>
2011-12-04 01:19:14 +0100

    first commit

---
+++ SimplePassword.Tests/CreateAndVerifyPassword.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Should.Fluent;
+using System.Security.Cryptography;
+
+namespace SimplePassword.Tests
+{
+    class CreateAndVerifyPassword
+    {
+        [Fact]
+        public void CanVerifyPasswordCreatedWithClearTextAgainstPasswordCreatedFromCorrectHashAndSalt()
+        {
+            // Arrange
+            var password = "testpassword";
+            
+            // Act
+            var passwordHash = new SaltedPasswordHash(password);
+            var verificationHash = new SaltedPasswordHash(passwordHash.Hash, passwordHash.Salt);
+            bool valid = verificationHash == passwordHash;
+
+            // Assert
+            valid.Should().Be.True();
+        }
+
+        [Fact]
+        public void CanNotVerifyTwoIdenticalPasswordsCreatedFromClearText()
+        {
+            // Arrange
+            var password = "testpassword";
+            var password2 = "testpassword";
+
+            // Act
+            var passwordHash1 = new SaltedPasswordHash(password);
+            var passwordHash2 = new SaltedPasswordHash(password2);
+
+            bool valid = passwordHash1 == passwordHash2;
+
+            // Assert
+            valid.Should().Be.False();
+        }
+
+        [Fact]
+        public void DoesNotVerifyIncorrectPassword()
+        {
+            // Arrange
+            var password = "testpassword";
+            var password2 = "boguspassword";
+
+            // Act
+            var passwordHash1 = new SaltedPasswordHash(password);
+            var passwordHash2 = new SaltedPasswordHash(password2);
+
+            bool valid = passwordHash1 == passwordHash2;
+
+            // Assert
+            valid.Should().Be.False();
+        }
+
+        [Fact]
+        public void CanDetermineIfHashedNotEqual()
+        {
+            // Arrange
+            var password = "testpassword";
+            var password2 = "boguspassword";
+
+            // Act
+            var passwordHash1 = new SaltedPasswordHash(password);
+            var passwordHash2 = new SaltedPasswordHash(password2);
+
+            bool valid = passwordHash1 != passwordHash2;
+
+            // Assert
+            valid.Should().Be.True();
+        }
+    }
+}
---
+++ SimplePassword/SaltedPasswordHash.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SimplePassword
+{
+    public class SaltedPasswordHash
+    {
+        private readonly string _hash;
+        private readonly string _salt;
+        private readonly HashAlgorithm _algorithm;
+        private readonly RandomNumberGenerator _randomNumberGenerator;
+        private readonly int _saltSize;
+
+        private const int DefaultSaltSize = 512;
+
+        public string Hash { get { return _hash; } }
+        public string Salt { get { return _salt; } }
+
+        public SaltedPasswordHash(string password, int saltSize = DefaultSaltSize, HashAlgorithm algorithm = null, RandomNumberGenerator randomNumberGenerator = null)
+        {
+            if (algorithm == null)
+                _algorithm = new SHA512Managed();
+            else
+                _algorithm = algorithm;
+
+            if (randomNumberGenerator == null)
+                _randomNumberGenerator = new RNGCryptoServiceProvider();
+            else
+                _randomNumberGenerator = randomNumberGenerator;
+
+            _saltSize = saltSize;
+
+            _salt = CreateSalt();
+            _hash = CreateHash(password);
+        }
+
+        public SaltedPasswordHash(string hash, string salt)
+        {
+            _hash = hash;
+            _salt = salt;
+            _saltSize = DefaultSaltSize;
+        }
+
+        private string CreateSalt()
+        {
+            byte[] saltBuffer = new byte[_saltSize];
+            _randomNumberGenerator.GetNonZeroBytes(saltBuffer);
+            return Convert.ToBase64String(saltBuffer);
+        }
+
+        private string CreateHash(string password)
+        {
+            byte[] saltAndPassword = Encoding.Unicode.GetBytes(String.Concat(_salt, password));
+            byte[] hashBuffer = _algorithm.ComputeHash(saltAndPassword);
+            return Encoding.Unicode.GetString(hashBuffer);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            var otherHash = obj as SaltedPasswordHash;
+            if (otherHash == null)
+                return false;
+
+            return Hash.Equals(otherHash.Hash);
+        }
+
+        public override int GetHashCode()
+        {
+            return Hash.GetHashCode();
+        }
+
+        public static bool operator ==(SaltedPasswordHash first, SaltedPasswordHash second)
+        {
+            if (System.Object.ReferenceEquals(first, second))
+                return true;
+
+            if (((object)first == null) || ((object)second == null))
+                return false;
+
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(SaltedPasswordHash first, SaltedPasswordHash second)
+        {
+            return !(first == second);
+        }
+    }
+}
